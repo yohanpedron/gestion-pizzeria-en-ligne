2bed3ad901c80e410dd4213366a555fe90da4c53
marion.bialgues <marion.bialgues@gmail.com>
2026-03-23 14:38:56 +0100

    WIP: création class Pizza et Ingredient

---
+++ src/main/java/com/accenture/model/Ingredient.java
@@ -0,0 +1,25 @@
+package com.accenture.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Ingredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+    private Integer stock;
+}
---
+++ src/main/java/com/accenture/model/Pizza.java
@@ -0,0 +1,28 @@
+package com.accenture.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.testcontainers.shaded.org.checkerframework.checker.units.qual.A;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Pizza {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private Map<PizzaSize, Double> price;
+    private List<Ingredient> ingredients;
+    private boolean active;
+}
