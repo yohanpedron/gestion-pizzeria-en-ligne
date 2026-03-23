85bc097bce0a325270dd4270889178c6aabae6eb
yohan.pedron <yohanpedron@gmail.com>
2026-03-23 09:29:40 +0100

    Premier commit

---
+++ src/main/java/com/accenture/gestionpizzeriaenligne/GestionPizzeriaEnLigneApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.gestionpizzeriaenligne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPizzeriaEnLigneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionPizzeriaEnLigneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/accenture/gestionpizzeriaenligne/GestionPizzeriaEnLigneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.gestionpizzeriaenligne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPizzeriaEnLigneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
