6142a0d87c855c77f7f957fe1a2667b06e0c632c
marion.bialgues <marion.bialgues@gmail.com>
2026-03-25 16:41:54 +0100

    TI classique et EndToEnd OK pour addPIzza

--- src/main/java/com/accenture/controller/Impl/PizzaController.java
+++ src/main/java/com/accenture/controller/Impl/PizzaController.java
@@ -1,14 +1,30 @@
 package com.accenture.controller.Impl;
 
+
 import com.accenture.service.PizzaService;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.AllArgsConstructor;
+import com.accenture.service.dto.PizzaRequestDto;
+import com.accenture.service.dto.PizzaResponseDto;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@AllArgsConstructor
+@RequestMapping("/pizza")
 public class PizzaController {
 
-    private final PizzaService pizzaService;
+    @Autowired
+    private PizzaService pizzaService;
+
+    @PostMapping
+    public ResponseEntity<PizzaResponseDto> addPizza(@Valid @RequestBody PizzaRequestDto request) {
+        PizzaResponseDto dto = pizzaService.addPizza(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(dto);
+    }
+
 }
+
---
+++ src/test/java/com/accenture/controller/PizzaControllerIntegrationEndToEnd.java
@@ -0,0 +1,49 @@
+package com.accenture.controller;
+
+import com.accenture.model.PizzaSize;
+import com.accenture.service.dto.IngredientRequestDto;
+import com.accenture.service.dto.PizzaRequestDto;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.TestRestTemplate;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+import java.util.Map;
+
+// on lance l'appli spring dans un contexte de test pour ce endpoint en particulier
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// permet à la config du serveur émetteur de la requete avec le serveur receveur selon les
+//normes REST
+@AutoConfigureTestRestTemplate
+//identifie un context d'éxecution de test avec les fichiers de config associé
+// ex: application-test.yml qui va charger le driver H2 et non le driver Postgres courant/nominal
+@ActiveProfiles("test")
+class PizzaControllerIntegrationEndToEndTest {
+
+    private static final String API_PIZZA_ENDPOINT = "/pizza";
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    @DisplayName("Creates pizza through Post endpoint")
+    void testPostPizzaSuccess() {
+        PizzaRequestDto pizzaRequestDto = new PizzaRequestDto("Savoyarde", Map.of(PizzaSize.LARGE, 14.0), List.of(new IngredientRequestDto("Reblochon")));
+
+        ResponseEntity<Void> response = restTemplate.postForEntity("http://localhost:"+port+API_PIZZA_ENDPOINT, pizzaRequestDto, Void.class);
+
+        Assertions.assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    }
+
+}
